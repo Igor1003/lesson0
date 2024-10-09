e6af9ac077fcad4990d26daec1bc968a99248f1c
Igor1003 <i.iwan4encko@gmail.com>
2024-10-10 01:53:05 +0700

    Add files via upload

---
+++ module_1_4.11111.py
@@ -0,0 +1,9 @@
+
+my_string = len("произвольный текст: ")
+print(my_string)
+
+my_string = "произвольный текст: "
+print(str(my_string).upper())
+print(str(my_string.lower()))
+print(str(my_string.replace(" ","")))
+print(str(my_string[-1:])) #Последний символ строки это пробел.
